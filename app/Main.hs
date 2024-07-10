93409b5db68143a9b8d072e88272a29f9c263b70
Sasaki Touma <netst915@gmail.com>
2024-07-10 22:27:28 +0900

    java checker

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,40 @@
 module Main (main) where
 
-import Lib
+import Control.Exception (try)
+import Control.Monad.Trans.Class (lift)
+import Control.Monad.Trans.Except (ExceptT, runExceptT, throwE)
+import GHC.IO.Exception (ExitCode (ExitSuccess))
+import System.Directory (createDirectoryIfMissing)
+import System.FilePath ((</>))
+import System.IO.Error (ioeGetErrorString)
+import System.Process (waitForProcess, runProcess)
+
+data LauncherError = JavaNotFound String
+
+instance Show LauncherError where
+    show (JavaNotFound s) = "The launcher could not find a Java executable: " ++ s
+
+makeWorkingFolder :: ExceptT LauncherError IO ()
+makeWorkingFolder = do
+    _ <- lift $ createDirectoryIfMissing False ("." </> "run")
+    return ()
+
+checkJavaExistence :: ExceptT LauncherError IO ()
+checkJavaExistence = do
+    process <- lift $ try (runProcess "java" ["-version"] Nothing Nothing Nothing Nothing Nothing)
+    case process of
+        Right handler ->
+            lift (waitForProcess handler) >>= \case
+                ExitSuccess -> return ()
+                exitCode    -> throwE (JavaNotFound (show exitCode))
+
+        Left e ->
+            throwE (JavaNotFound (ioeGetErrorString e))
+
+program :: ExceptT LauncherError IO ()
+program = do
+    makeWorkingFolder
+    checkJavaExistence
 
 main :: IO ()
-main = someFunc
+main = runExceptT program >>= either print (const (return ()))
