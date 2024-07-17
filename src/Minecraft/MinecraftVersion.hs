d16eeaff909dd0c1ba19fe00a8d8a85303a40c84
Sasaki Touma <netst915@gmail.com>
2024-07-17 12:36:27 +0900

    improve minecraft version parser

--- src/Minecraft/MinecraftVersion.hs
+++ src/Minecraft/MinecraftVersion.hs
@@ -1,4 +1,4 @@
-module Minecraft.MinecraftVersion (MinecraftVersion(..)) where
+module Minecraft.MinecraftVersion (MinecraftVersion(..), parseMinecraftVersion) where
 
 import           Data.Text        (unpack)
 import           Data.Yaml        (FromJSON (..), Value (..))
@@ -7,21 +7,31 @@ import           Text.Regex.Posix ((=~))
 data MinecraftVersion = MinecraftVersion Int Int Int
 
 instance Show MinecraftVersion where
+    show (MinecraftVersion major minor 0) =
+        show major ++ "." ++ show minor
     show (MinecraftVersion major minor patch) =
         show major ++ "." ++ show minor ++ "." ++ show patch
 
 instance FromJSON MinecraftVersion where
-    parseJSON (String txt) = let str = unpack txt in
-        if str =~ "[0-9]+\\.[0-9]+\\.[0-9]+$" then
-            let (major, str')  = takeWhileAndRemains (/= '.') str
-                (minor, patch) = takeWhileAndRemains (/= '.') str'
-            in
-            return (MinecraftVersion (read major) (read minor) (read patch))
-        else
-            fail "Unrecognisable minecraft version"
-        where
-            takeWhileAndRemains f s =
-                let taken = takeWhile f s in
-                    (taken, drop (length taken + 1) s)
+    parseJSON (String txt) =
+        case parseMinecraftVersion (unpack txt) of
+            Just v  -> return v
+            Nothing -> fail "Unrecognisable minecraft version"
 
     parseJSON _ = fail "Unrecognisable minecraft version"
+
+parseMinecraftVersion :: String -> Maybe MinecraftVersion
+parseMinecraftVersion str
+    | str =~ "[0-9]+\\.[0-9]+\\.[0-9]+$" =
+        let (major, str')  = takeWhileAndRemains (/= '.') str
+            (minor, patch) = takeWhileAndRemains (/= '.') str' in
+            Just (MinecraftVersion (read major) (read minor) (read patch))
+    | str =~ "[0-9]+\\.[0-9]+$" =
+        let (major, str') = takeWhileAndRemains (/= '.') str
+            (minor, _)    = takeWhileAndRemains (/= '.') str' in
+            Just (MinecraftVersion (read major) (read minor) 0)
+    | otherwise = Nothing
+    where
+        takeWhileAndRemains f s =
+            let taken = takeWhile f s in
+                (taken, drop (length taken + 1) s)
