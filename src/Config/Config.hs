fc1ecaa1f289120806f3e48da68dff05446a28fc
Sasaki Touma <netst915@gmail.com>
2024-07-23 12:25:08 +0900

    flexible config

--- src/Config/Config.hs
+++ src/Config/Config.hs
@@ -5,9 +5,12 @@ module Config.Config
     , ClientConfig(..)
     ) where
 
+import           Config.Resource
+
 import           Data.Minecraft.MCServerBrand (MCServerBrand)
-import           Data.Minecraft.MCVersion     (MCVersion)
-import           Data.Yaml                    (FromJSON (..), Value (..), (.:))
+import           Data.Minecraft.MCVersion     (MCVersion (..))
+import           Data.Yaml                    (FromJSON (..), Value (..), (.!=),
+                                               (.:))
 
 data Config = Config
     { applicationConfig :: ApplicationConfig
@@ -16,11 +19,34 @@ data Config = Config
     }
     deriving Show
 
+instance FromJSON Config where
+    parseJSON (Object m) =
+        Config
+            <$> m .: "application" .!= defaultApplicationConfig
+            <*> m .: "server"      .!= defaultServerConfig
+            <*> m .: "client"      .!= defaultClientConfig
+
+    parseJSON _ = fail "Unrecognisable config"
+
 data ApplicationConfig = ApplicationConfig
     { workingDir       :: FilePath
     , autoexecCommands :: [String]
     }
     deriving Show
+
+instance FromJSON ApplicationConfig where
+    parseJSON (Object m) =
+        ApplicationConfig
+            <$> m .: "workingDir" .!= defaultApplicationWorkingDir
+            <*> m .: "autoexec"   .!= defaultApplicationAutoexec
+
+    parseJSON _ = fail "Unrecognisable application config"
+
+defaultApplicationConfig :: ApplicationConfig
+defaultApplicationConfig =
+    ApplicationConfig
+        defaultApplicationWorkingDir
+        defaultApplicationAutoexec
 
 data ServerConfig = ServerConfig
     { serverBrand         :: MCServerBrand
@@ -29,6 +55,24 @@ data ServerConfig = ServerConfig
     , serverStaticPlugins :: [FilePath]
     }
     deriving Show
+
+instance FromJSON ServerConfig where
+    parseJSON (Object m) =
+        ServerConfig
+            <$> m .: "brand"         .!= defaultServerBrand
+            <*> m .: "version"       .!= defaultServerVersion
+            <*> m .: "jvmOptions"    .!= defaultServerJvmOptions
+            <*> m .: "staticPlugins" .!= defaultServerStaticPlugins
+
+    parseJSON _ = fail "Unrecognisable server config"
+
+defaultServerConfig :: ServerConfig
+defaultServerConfig =
+    ServerConfig
+        defaultServerBrand
+        defaultServerVersion
+        defaultServerJvmOptions
+        defaultServerStaticPlugins
 
 data ClientConfig = ClientConfig
     { clientDefaultVersion :: MCVersion
@@ -39,34 +83,13 @@ data ClientConfig = ClientConfig
 instance FromJSON ClientConfig where
     parseJSON (Object m) =
         ClientConfig
-            <$> m .: "defaultVersion"
-            <*> m .: "jvmOptions"
+            <$> m .: "defaultVersion" .!= defaultClientDefaultVersion
+            <*> m .: "jvmOptions"     .!= defaultClientJvmOptions
 
     parseJSON _ = fail "Unrecognisable client config"
 
-instance FromJSON ServerConfig where
-    parseJSON (Object m) =
-        ServerConfig
-            <$> m .: "brand"
-            <*> m .: "version"
-            <*> m .: "jvmOptions"
-            <*> m .: "staticPlugins"
-
-    parseJSON _ = fail "Unrecognisable server config"
-
-instance FromJSON ApplicationConfig where
-    parseJSON (Object m) =
-        ApplicationConfig
-            <$> m .: "workingDir"
-            <*> m .: "autoexec"
-
-    parseJSON _ = fail "Unrecognisable application config"
-
-instance FromJSON Config where
-    parseJSON (Object m) =
-        Config
-            <$> m .: "application"
-            <*> m .: "server"
-            <*> m .: "client"
-
-    parseJSON _ = fail "Unrecognisable config"
+defaultClientConfig :: ClientConfig
+defaultClientConfig =
+    ClientConfig
+        defaultClientDefaultVersion
+        defaultClientJvmOptions
--- src/Config/Resource.hs
+++ src/Config/Resource.hs
@@ -1,9 +1,57 @@
 {-# LANGUAGE TemplateHaskell #-}
 
-module Config.Resource (defaultConfigFile) where
+module Config.Resource
+    ( defaultConfigFile
+    , defaultApplicationWorkingDir
+    , defaultApplicationAutoexec
+    , defaultServerBrand
+    , defaultServerVersion
+    , defaultServerJvmOptions
+    , defaultServerStaticPlugins
+    , defaultClientDefaultVersion
+    , defaultClientJvmOptions
+    ) where
 
-import           Data.ByteString (ByteString)
-import           Data.FileEmbed  (embedFile)
+import           Data.ByteString              (ByteString)
+import           Data.FileEmbed               (embedFile)
+import           Data.Minecraft.MCServerBrand (MCServerBrand (..))
+import           Data.Minecraft.MCVersion     (MCVersion (..))
 
 defaultConfigFile :: ByteString
 defaultConfigFile = $(embedFile ".BukkitDevelopREPL.yaml")
+
+defaultApplicationWorkingDir :: FilePath
+defaultApplicationWorkingDir = "./BukkitDevelopREPL"
+
+defaultApplicationAutoexec :: [String]
+defaultApplicationAutoexec = []
+
+defaultServerBrand :: MCServerBrand
+defaultServerBrand = Paper
+
+defaultServerVersion :: MCVersion
+defaultServerVersion = MCVersion 1 20 1
+
+defaultServerJvmOptions :: [String]
+defaultServerJvmOptions =
+    [ "-Xms2G"
+    , "-Xmx2G"
+    ]
+
+defaultServerStaticPlugins :: [FilePath]
+defaultServerStaticPlugins = []
+
+defaultClientDefaultVersion :: MCVersion
+defaultClientDefaultVersion = MCVersion 1 20 1
+
+defaultClientJvmOptions :: [String]
+defaultClientJvmOptions =
+    [ "-Xms2G"
+    , "-Xmx2G"
+    , "-XX:+UnlockExperimentalVMOptions"
+    , "-XX:+UseG1GC"
+    , "-XX:G1NewSizePercent=20"
+    , "-XX:G1ReservePercent=20"
+    , "-XX:MaxGCPauseMillis=50"
+    , "-XX:G1HeapRegionSize=32M"
+    ]
